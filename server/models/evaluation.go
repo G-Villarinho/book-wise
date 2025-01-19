a99ce8308b8ce89dcddf14b7a1c8da60be5303cf
Gabriel Villarinho <gabrielvillarinho01@gmail.com>
2025-01-19 01:29:08 -0300

    feat: add Evaluation model with fields for rating, description, user ID, and book ID

---
+++ server/models/evaluation.go
@@ -0,0 +1,17 @@
+package models
+
+import "github.com/google/uuid"
+
+type Evaluation struct {
+	BaseModel
+	Rate        uint8     `gorm:"column:Published;type:TINYINT;not null;default:0"`
+	Description string    `gorm:"column:Description;type:varchar(2000);not null"`
+	UserID      uuid.UUID `gorm:"column:UserId;type:char(36);not null"`
+	BookID      uuid.UUID `gorm:"column:BookId;type:char(36);not null"`
+	User        User      `gorm:"foreignKey:UserID;references:ID"`
+	Book        Book      `gorm:"foreignKey:BookID;references:ID"`
+}
+
+func (e *Evaluation) TableName() string {
+	return "Evaluations"
+}
