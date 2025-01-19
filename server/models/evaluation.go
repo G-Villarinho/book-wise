fc6a4aa903037539fa16be25b4bdb75add22138d
Gabriel Villarinho <gabrielvillarinho01@gmail.com>
2025-01-19 15:30:43 -0300

    fix: correct column name for Rate in Evaluation model

--- server/models/evaluation.go
+++ server/models/evaluation.go
@@ -4,7 +4,7 @@ import "github.com/google/uuid"
 
 type Evaluation struct {
 	BaseModel
-	Rate        uint8     `gorm:"column:Published;type:TINYINT;not null;default:0"`
+	Rate        uint8     `gorm:"column:Rate;type:TINYINT;not null;default:0"`
 	Description string    `gorm:"column:Description;type:varchar(2000);not null"`
 	UserID      uuid.UUID `gorm:"column:UserId;type:char(36);not null"`
 	BookID      uuid.UUID `gorm:"column:BookId;type:char(36);not null"`
